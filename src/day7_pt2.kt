9142a53461d6cb9273f6699b2f7a177dcb1f9e4d
John Godfrey <jgodfr04@dcs.bbk.ac.uk>
2020-01-19 22:51:37 +0000

    Day 7 part 2 complete

--- src/day7_pt2.kt
+++ src/day7_pt2.kt
@@ -1,31 +1,39 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     println("First test passes: ${
     getOutputFromFeedbackLoop(
         "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
     ) == 139629729
     }")
-//    println("Second test passes: ${
-//    getOutputFromFeedbackLoop(
-//        "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
-//    ) == 18216
-//    }")
+    println("Second test passes: ${
+    getOutputFromFeedbackLoop(
+        "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
+    ) == 18216
+    }")
+    println("From input we get: ${getOutputFromFeedbackLoop(File("input_day7").readText())}")
 }
 
-fun getOutputFromFeedbackLoop(tape: String): Int {
+fun getOutputFromFeedbackLoop(tape: String): Int? {
     val program = tape.split(",").map { it.toInt() }
-    val everyCombinationOfPhaseSettings = getEveryCombinationOfCharacters("56789")
-    val amplifiers = "98765".map {
-        getContextForPhaseSetting(it, program)
-    }
-    return processContexts(amplifiers)
+
+    return getEveryCombinationOfCharacters("56789")
+        .map { str ->
+            str.map {
+                getContextForPhaseSetting(it, program)
+            }
+        }
+        .map { processContexts(it) }
+        .max()
 }
+
+data class Context(val tape: List<Int>, val offset: Int, val io: Int, val lastOpcode: Int = 0)
 
 fun processContexts(amplifiers: List<Context>): Int {
     var rotatingList = amplifiers
     while (!rotatingList.all { it.lastOpcode == 99 }) {
         rotatingList = rotatingList.drop(1) + listOf(computeForSingleInput(rotatingList.first().copy(io = rotatingList.last().io)))
     }
-    println("After rotating, rotatingList: $rotatingList")
     return rotatingList.last().io
 }
 
@@ -33,8 +41,6 @@ fun getContextForPhaseSetting(setting: Char, tape: List<Int>): Context {
     val phaseSetting = Integer.parseInt(setting.toString())
     return computeForSingleInput(Context(tape, 0, phaseSetting))
 }
-
-data class Context(val tape: List<Int>, val offset: Int, val io: Int, val lastOpcode: Int = 0)
 
 fun computeForSingleInput(input: Context): Context  {
     var opcode = input.tape[input.offset]
@@ -49,7 +55,6 @@ fun computeForSingleInput(input: Context): Context  {
     }
 
     while (!(inputConsumed && opcode == 3) && opcode != 99) {
-        println(input)
         val result = when (opcode % 10) {
             1 -> operation(Op.ADD, opcode / 100, pointerPosition, tapeModified)
             2 -> operation(Op.MUL, opcode / 100, pointerPosition, tapeModified)
